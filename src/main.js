368f9758d45105a16628a5671684433d14adbc83
jeremy <jeremy.froment@educlever.com>
2021-12-06 18:11:24 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '../node_modules/tailwindcss/dist/tailwind.min.css'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
